95e1900d8e781a721adfab1d7e7f63684c67883f
Berlin Sky <pedroyoung8989@gmail.com>
2017-06-10 21:33:24 -0700

    set up

---
+++ app/ts/index.ts
@@ -0,0 +1,11 @@
+"use strict";
+
+const words: string[] = ['hello', 'world'];
+words.forEach(word => console.log(word));
+
+console.log('here are some number: ');
+[1, 2, 3].map(n => n ** 2).forEach(console.log)
+
+console.log(`et voilà!`);
+
+console.log('--- END several modules used and no one hurt ---')
