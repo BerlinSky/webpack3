6cc3f7ac1d9ced8108726085fe3b6c04d85f88f8
Berlin Sky <pedroyoung8989@gmail.com>
2018-02-04 15:00:44 -0800

    add angular module

--- app/index.js
+++ app/index.js
@@ -1,18 +1,26 @@
-import 'jquery';
+// import 'jquery';
+import angular from 'angular';
 
-// eslint-disable-next-line
+// // eslint-disable-next-line
 
-const siteSetup = () => {
-  console.log("Set up ... in arrow function now")
-}
+const app = angular.module('myApp', []);
 
-const testjQuery = () => {
-  const thisBody = $('body');
-  thisBody.css({ 'backgroundColor': 'tomato' })
-}
-
-$(function () {
-  siteSetup();
-  testjQuery();
+app.controller('myCtrl', function($scope) {
+    $scope.firstName= "John";
+    $scope.lastName= "Doe";
 });
+
+// const siteSetup = () => {
+//   console.log("Set up ... in arrow function now")
+// }
+
+// const testjQuery = () => {
+//   const thisBody = $('body');
+//   thisBody.css({ 'backgroundColor': 'tomato' })
+// }
+
+// $(function () {
+//   siteSetup();
+//   testjQuery();
+// });
 
